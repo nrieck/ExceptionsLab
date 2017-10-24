82dcb93488040fef67e851d66784604c085b9adc
Nick <Nick@192.168.0.77>
2017-10-24 02:34:23 -0500

    Worked on lab 1

--- src/lab1/Challenge1.java
+++ src/lab1/Challenge1.java
@@ -16,28 +16,61 @@ import javax.swing.JOptionPane;
  * @version 1.00
  */
 public class Challenge1 {
-    private static final int LAST_NAME_IDX = 1;
+    private int lastNameIndex = 1;
+    private static String inputMessageDialog = "Enter full name (use Format: first last):";
 
     // this method responsible for output
     public static void main(String[] args) {
         Challenge1 app = new Challenge1();
         
-        String fullName = JOptionPane.showInputDialog("Enter full name (use Format: first last):");
-        String lastName = app.extractLastName(fullName);
-        String msg = "Your last name is: " + lastName;
-        JOptionPane.showMessageDialog(null, msg);
+        String fullName = JOptionPane.showInputDialog(inputMessageDialog);
+        String lastName = "";
+        try {
+            lastName = app.extractLastName(fullName);
+            String msg = "Your last name is: " + lastName;
+            JOptionPane.showMessageDialog(null, msg);
+        } catch (IllegalArgumentException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        
     }
     
     // write the code to extract the lastName from the fullName
     // Use exception handling to prevent a crash in the event that fullName
     // is null or empty. Throw the exception to the calling method. and handle
     // it there.
-    public String extractLastName(String fullName) {
+    public String extractLastName(String fullName) throws IllegalArgumentException {
         String lastName = null;
         
-        // Your code goes here.
+        if(fullName == null || fullName.isEmpty() ) {
+            throw new IllegalArgumentException("You have entered an invalid full name");
+        }       
+        String[] splitName = fullName.split("\\s+");
+        lastName = splitName[lastNameIndex];
         
-        return lastName;
+        return lastName;     
+    }
+
+    public int getLastNameIndex() {
+        return lastNameIndex;
+    }
+
+    public void setLastNameIndex(int lastNameIndex) {
+        if(lastNameIndex < 0) {
+            throw new IllegalArgumentException("You have entered an invalid last name");
+        }
+        this.lastNameIndex = lastNameIndex;
+    }
+
+    public static String getInputMessageDialog() {
+        return inputMessageDialog;
+    }
+
+    public static void setInputMessageDialog(String inputMessageDialog) {
+        if(inputMessageDialog == null || inputMessageDialog.isEmpty()) {
+            throw new IllegalArgumentException("You have entered an invalid message Dialog");
+        }
+        Challenge1.inputMessageDialog = inputMessageDialog;
     }
 
 }
